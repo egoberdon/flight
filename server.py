ceda507928d52503daf182fb9bb99a51cc20a950
Eli Goberdon <goberdon@gmail.com>
2016-05-19 00:10:27 -0400

    Flask controller now loads a page asking user for entries, on entry it loads a new page with the entries echoed back

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def result():
+    category = request.form["category"]
+    distance = request.form["distance"]
+    center   = request.form["center"]
+    return "Category is %s, Distance is %s, Center is %s." % (category, distance, center)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
