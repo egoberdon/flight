ab62e39e663e6f633ae9e65138d3ecbce33991d6
Eli Goberdon <goberdon@gmail.com>
2016-05-21 17:58:21 -0400

    added database code

--- server.py
+++ server.py
@@ -1,12 +1,41 @@
-from flask import Flask, render_template, request
+import sqlite3
+from flask import Flask, render_template, request, g
+from contextlib import closing
 
 app = Flask(__name__, static_url_path='')
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def result():
     category = request.form["category"]
     distance = request.form["distance"]
